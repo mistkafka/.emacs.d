8ef8b952cb9530cf1d0a50148e480b663867def6
mistkafka <mistkafka@gmail.com>
2018-04-07 02:23:06 +0800

    使用json-storage迁移了原本的switch-project，并且支持手动注册一个git project

--- lisp/init-git-project.el
+++ lisp/init-git-project.el
@@ -2,6 +2,7 @@
 
 (require-package 's)
 (require 'json-storage)
+(require 'seq)
 
 (defun mistkafka/git-project/get-git-root-path (&optional current-path)
   "get the CURRENT-PATH's git root path"
@@ -66,10 +67,10 @@
   )
 
 (defun mistkafka/git-project/get-file-name ()
-    (let ((filename (if (equal major-mode 'dired-mode)
+  (let ((filename (if (equal major-mode 'dired-mode)
                       default-directory
                     (buffer-file-name))))
-      filename))
+    filename))
 
 (defun mistkafka/git-project/get-file-name-in-project ()
   "Get current buffer file name in the current git project"
@@ -101,30 +102,33 @@
 (defvar mistkafka/git-project/cache-file-path "/Users/mistkafka/.emacs.d/.cache/git-projects"
   "缓存文件的路径")
 
-(defconst mistkafka/git-project/cache-storage (json-storage/create-storage mistkafka/git-project/cache-file-path))
+(defconst mistkafka/git-project/cache-storage (json-storage/create-storage
+					       :path mistkafka/git-project/cache-file-path
+					       :schema-alist '(
+							       ("projects-from-locate"     . [])
+							       ("projects-from-manual-reg" . [])
+							       )))
 
 (defun mistkafka/git-project/read-projects-cache ()
   "从缓存文件中读取projects，并返回。"
-  (json-storage/get-value mistkafka/git-project/cache-storage))
+  (json-storage/get-value "projects-from-locate" mistkafka/git-project/cache-storage))
 
-(defun mistkafka/git-project/update-projects-cache ()
-  "更新projects的缓存"
+(defun mistkafka/git-project/update-cache-of-projects-from-locate ()
+  "执行一遍`locate'命令来刷新`projects-from-locate'的缓存"
   (interactive)
   (let (
 	(projects (mistkafka/git-project/do-update-projects-cache))
-	(hash (make-hash-table :test 'equal))
 	)
-    (puthash "projects" projects hash)
-    (json-storage/update-value mistkafka/git-project/cache-storage hash)
+    (json-storage/set-value "projects-from-locate" projects mistkafka/git-project/cache-storage)
     )
   (message "更新成功！"))
 
 (defun mistkafka/git-project/do-update-projects-cache ()
   "负责执行projects缓存的更新"
   (let* ((cmd-tpl "locate .git | grep '^/Users/mistkafka/Code/\\(%s\\).*\\.git$' ")
-	(locate-paths (s-join "\\|" mistkafka/git-project/locate-paths-of-code))
-	(cmd (format cmd-tpl locate-paths))
-	(result-str (shell-command-to-string cmd)))
+	 (locate-paths (s-join "\\|" mistkafka/git-project/locate-paths-of-code))
+	 (cmd (format cmd-tpl locate-paths))
+	 (result-str (shell-command-to-string cmd)))
     (setq result-str (s-replace-regexp "/\\.git$" "" result-str))
     (with-temp-file mistkafka/git-project/cache-file-path
       (delete-region (point-min) (point-max))
@@ -142,18 +146,59 @@
   (interactive "P")
   
   (when should-update-cache?
-    (mistkafka/git-project/update-projects-cache))
+    (mistkafka/git-project/update-cache-of-projects-from-locate))
   
   (let* (
-	 (cache-hash (json-storage/get-value mistkafka/git-project/cache-storage))
-	 (projects (gethash "projects" cache-hash))
+	 projects
 	 selected
 	 )
+    (setq projects (vconcat (json-storage/get-value "projects-from-locate" mistkafka/git-project/cache-storage)
+			    (json-storage/get-value "projects-from-manual-reg" mistkafka/git-project/cache-storage)))
+    (setq projects (append projects nil))
+    (setq projects (delete-dups projects))
     (setq selected (ivy-read "Switch to Project: " projects))
     (when selected
       (find-file selected)
       (goto-char (point-min))
       (mistkafka/git-project/find-file))
+    ))
+
+(defun mistkafka/git-project/registe-current-project ()
+  "把当前的git项目注册到project列表里。
+有些时候一些git项目并不在~/Code目录下，用locate去搜索很耗时，所以采用这种手动注册的方式来进行。"
+  (interactive)
+  (let (
+	(git-root-path (mistkafka/git-project/get-git-root-path))
+	(manual-reg-projects (json-storage/get-value "projects-from-manual-reg" mistkafka/git-project/cache-storage))
+	 )
+    
+    (if (and git-root-path
+	     (not (seq-contains manual-reg-projects git-root-path)))
+	(progn
+	  (json-storage/set-value "projects-from-manual-reg" (vconcat manual-reg-projects (vector git-root-path)) mistkafka/git-project/cache-storage)
+	  (message "注册成功！"))
+      (message "未选择，或已经存在！"))
+    )
+  )
+
+(defun mistkafka/git-project/remove-registed-project ()
+  (interactive)
+  (let* (
+	 (manual-reg-projects (json-storage/get-value "manual-reg-projects" mistkafka/git-project/cache-storage))
+	 selected
+	 )
+    (if (and manual-reg-projects
+	     (/= 0 (length manual-reg-projects)))
+	(progn
+	  (setq selected (ivy-read "remove which:"
+				   (append manual-reg-projects nil) ; cover vector to list
+				   ))
+	  (setq manual-reg-projects (seq-remove selected manual-reg-projects))
+	  (setq manual-reg-projects (vconcat [] manual-reg-projects)) ; cover list to vector
+	  (json-storage/set-value "projects-from-manual-reg" manual-reg-projects mistkafka/git-project/cache-storage)
+	  (message (format "删除'%s'成功" selected))
+	  )
+      (message "没有注册过project"))
     ))
 
 (mistkafka/keyboard/bind "pf" 'mistkafka/git-project/find-file)
