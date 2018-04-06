11cc6229ac7735110bf5eb8dff808a0a64b3e04b
mistkafka <mistkafka@gmail.com>
2018-04-07 02:22:41 +0800

    优化了json-storage的使用方式

--- lisp/json-storage.el
+++ lisp/json-storage.el
@@ -1,53 +1,73 @@
-(require 'dash)
+(require 'cl-macs)
 (require 'json)
 
-(defun json-storage/create-storage (path)
-  "根据PATH创建一个storage，并从这个PATH中读值来初始化这个storage，最后返回这个storage。
-
-storage的path表示存储value的文件路径；
-storage的value表示storage的值（即缓存）；
-storage的is-expire表示value是否过期。"
-
+(cl-defun json-storage/create-storage (&key path schema-alist)
   (let (
 	(storage (make-hash-table :test 'equal))
+	value-in-file
+	(has-apply-default-value nil)
 	)
     (puthash "path" path storage)
-    (puthash "is-expire" t storage)
-    (puthash "value" (json-storage/get-value storage) storage)
-    storage))
+    (setq value-in-file (json-storage/get-json-from-file path))
+    (cl-loop for schema in schema-alist
+	     for key = (car schema)
+	     for init-value = (cdr schema)
+	     do (unless (gethash key value-in-file)
+		  (setq has-apply-default-value t)
+		  (puthash key init-value value-in-file))
+	     )
+    (puthash "value" value-in-file storage)
+    (when has-apply-default-value
+      (json-storage/save-to-file storage))
+    storage
+    ))
 
-(defun json-storage/get-value (storage)
-
-  (when (gethash "is-expire" storage)
-    (puthash "value" (json-storage/get-value-from-file (gethash "path" storage)) storage)
-    (puthash "is-expire" nil storage))
-  
-  (gethash "value" storage))
-
-(defun json-storage/get-value-from-file (path)
+(defun json-storage/get-json-from-file (path)
   (with-temp-buffer
     (let (
 	  (json-object-type 'hash-table)
-	  (json-array-type 'list)
+	  (json-array-type 'vector)
 	  (json-key-type 'string)
-	  file-content)
-      (insert-file-contents path)
-      (setq file-content (buffer-string))
-      (when (string= file-content "")
-	(setq file-content "{}"))
-      (json-read-from-string file-content)))
+	  (file-content ""))
+      
+      (when (file-exists-p path)
+	(insert-file-contents path)
+	(setq file-content (buffer-string)))
+      
+      (if (string= file-content "")
+	  (make-hash-table :test 'equal)
+	(json-read-from-string file-content))
+      )
+    )
   )
 
-(defun json-storage/update-value (storage value)
-
+(defun json-storage/save-to-file (storage)
   (with-temp-file (gethash "path" storage)
-    (puthash "value" value storage)
     (delete-region (point-min) (point-max))
     (let (
 	  (json-object-type 'hash-table)
-	  (json-array-type 'list)
-	  (json-key-type 'string))
+	  (json-array-type 'vector)
+	  (json-key-type 'string)
+	  (value (gethash "value" storage)))
       (insert (json-encode-hash-table value)))
-    storage))
+    (json-pretty-print-buffer-ordered)
+    )
+  storage
+  )
+
+(defun json-storage/get-value (key storage)
+  (let (
+	 (storage-value (gethash "value" storage))
+	 )
+    (gethash key storage-value)
+    ))
+
+(defun json-storage/set-value (key value storage)
+  (let (
+	(storage-value (gethash "value" storage))
+	)
+    (puthash key value storage-value)
+    (json-storage/save-to-file storage)
+    ))
 
 (provide 'json-storage)
