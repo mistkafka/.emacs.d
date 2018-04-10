14670275cd3ebc4689fd97bb6ce30ccd9d8ff61f
mistkafka <mistkafka@gmail.com>
2018-04-10 19:47:57 +0800

    magit 按键绑定

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -25,6 +25,17 @@
 (require-package 'magit)
 (mistkafka/keyboard/bind "gs" 'magit-status)
 (mistkafka/keyboard/bind "gp" 'magit-push-current-to-upstream)
+(mistkafka/keyboard/bind "ghb" 'magit-blame)
+
+(mistkafka/keyboard/bind "gbn" 'magit-branch-and-checkout)
+(mistkafka/keyboard/bind "gbc" 'magit-branch-checkout)
+
+(mistkafka/keyboard/bind "gmm" 'magit-merge)
+(mistkafka/keyboard/bind "gma" 'magit-merge-abort)
+(mistkafka/keyboard/bind "gmc" 'magit-merge) ;magit-merge-continue
+
+(mistkafka/keyboard/bind "grr" 'magit-rebase)
+(mistkafka/keyboard/bind "gri" 'magit-rebase-interactive)
 
 ;; 复制黏贴, 在终端里依然不work
 ;; (global-set-key (kbd "S-c") 'clipboard-kill-region)
