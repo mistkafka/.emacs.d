e60d9024db963ca13e7ab48a59c8e1d10e4d4193
mistkafka <mistkafka@gmail.com>
2018-04-25 14:16:38 +0800

    :sparkles: 将本地任务同步到habitica。TODO: 删除habitica上任务，只留下 e4882b88-e21a-4fb3-8bb1-37102ca203da，进行调试。它的同步总是失败。

--- lisp/mistkafka-habitica.el
+++ lisp/mistkafka-habitica.el
@@ -30,11 +30,11 @@
     ;; (message (format "*curl-request*, running request: %s" cmd))
     (setq result
           (shell-command-to-string cmd))
-    (message (format "*curl-request*, with result: %s" result))
+    ;; (message (format "*curl-request*, with result: %s" result))
     result
     ))
 
-(defun mistkafka/habitica/request (endpoint method &optional data)
+(defun mistkafka/habitica/request (endpoint &optional method data)
   "对`my/curl-request'进一步进行封装，封装成habitica所需要的request.
 ENDPOINT是habitica的接口链接。
 METHOD是http的method，post, get, put等。
@@ -120,13 +120,19 @@ everyX指的是频率"
 
 (defun my/get-current-headline-habitica-id ()
   "获取当前headline的habitica-id"
-  (org-entry-get (point) "HABITICATE-ID" nil t))
+  (org-entry-get (point) "HABITICA-ID" nil t))
 
 (setq difficulty-org-2-habitica-table (make-hash-table :test 'equal))
 (puthash "Trivial" 0.5 difficulty-org-2-habitica-table)
 (puthash "Easy"    1   difficulty-org-2-habitica-table)
 (puthash "Medium"  1.5 difficulty-org-2-habitica-table)
 (puthash "Hard"    2   difficulty-org-2-habitica-table)
+
+(puthash 0.5 "Trivial" difficulty-org-2-habitica-table)
+(puthash 1   "Easy"    difficulty-org-2-habitica-table)
+(puthash 1.5 "Medium"  difficulty-org-2-habitica-table)
+(puthash 2   "Hard"    difficulty-org-2-habitica-table)
+
 (defun my/get-current-headling-habitica-priority ()
   "获取当前headline的priority"
   (let ((tags (nth 5 (org-heading-components))))
@@ -275,7 +281,7 @@ everyX指的是频率"
            (gethash "habitica-id" headline-info))
       (progn
         (mistkafka/habitica/del-task (gethash "habitica-id" headline-info))
-        (org-delete-property "HABITICATE-ID")
+        (org-delete-property "HABITICA-ID")
         (message "Success: 成功删除habitica任务！")
         )
     (unless silence-error
@@ -295,5 +301,202 @@ everyX指的是频率"
   (mistkafka/habitica/request "/cron" "POST" nil))
 
 (add-hook 'org-after-todo-state-change-hook 'mistkafka/habitica/headline-state-change-callback)
+
+(defun mistkafka/habitica/get-habitica-tasks ()
+  (alist-get 'data (mistkafka/habitica/request "/tasks/user" "GET")))
+
+(defun mistkafka/habitica/cover-task-2-headline (task)
+  (let ((type (alist-get 'type task)))
+    (with-temp-buffer
+      (org-mode)
+      (insert "** TODO ")                 ; headline level
+      (insert (alist-get 'text task))     ; headline title
+      (insert "  ")                       ; prepare to add headline tag
+      
+      (insert (format ":%s:" (gethash (alist-get 'priority task) difficulty-org-2-habitica-table))) ; headline tag
+
+      (insert (format "\n\n %s" (alist-get 'notes task ""))) ; headline content
+      (when (string= "daily" type)
+        (insert "\n\n")
+        (mapc
+         (lambda (checkitem)
+           (insert (format " - [ ] %s\n" (alist-get 'text checkitem))))
+         (alist-get 'checklist task)))
+      (org-set-property "HABITICA-ID" (alist-get 'id task)) ; id
+      (org-set-property "HABITICA-TYPE" type)
+      
+      ;; headline deadline
+      (cond
+       ((string= type "todo")  (when (alist-get 'date task)
+                                 (org-deadline '(0) (mistkafka/habitica/cover-time-to-string (alist-get 'date task)))
+                                 ;; 不知道为什么 org-deadline会自己产生一个+1d
+                                 ))
+       ((string= type "daily") (progn
+                                 (org-deadline '(0) (mistkafka/habitica/cover-time-to-string (aref (alist-get 'nextDue task) 0)))
+                                 (org-back-to-heading)
+                                 (search-forward "DEADLINE")
+                                 (search-forward ">")
+                                 (backward-char)
+                                 (insert " +")
+                                 (insert (number-to-string (alist-get 'everyX task)))
+                                 (insert (substring (alist-get 'frequency task) 0 1))
+                                 ))
+       ((string= type "habit") ()))
+      
+      (org-back-to-heading)
+      (org-set-tags-command '(4) t)
+
+      (org-indent-region (point-min) (point-max))
+      (buffer-substring-no-properties (point-min) (point-max)))
+    ))
+
+(defun mistkafka/habitica/insert-headline-to-inbox (task)
+  (interactive)
+  (goto-char (org-find-property "IS-INBOX-HEADLINE"))
+  (goto-char (org-end-of-subtree))
+  (insert "\n")
+  (insert (mistkafka/habitica/cover-task-2-headline task)))
+
+(defun mistkafka/habitica/cover-time-to-string (the-date)
+  (when the-date
+    (format-time-string "%Y-%m-%d %H:%M:%S" (date-to-time the-date) t)
+    ))
+
+(defun mistkafka/habitica/is-the-same-between-headline-and-task (local-task remote-task)
+  (and (string= (gethash "title" local-task)
+                (alist-get 'title remote-task))
+       
+       (string= (gethash "notes" local-task)
+                (alist-get 'notes remote-task))
+
+       (string= (gethash "priority" local-task)
+                (number-to-string (alist-get 'priority remote-task)))
+
+       (cond
+        ((string= "todo"  (gethash "type" local-task)) (equal (gethash "date" local-task)
+                                                              (mistkafka/habitica/cover-time-to-string (alist-get 'date remote-task))))
+        ((string= "daily" (gethash "type" local-task)) (and (string= (gethash "startDate" local-task)
+                                                                     (mistkafka/habitica/cover-time-to-string (alist-get 'startDate remote-task)))
+                                                            (string= (gethash "frequency" local-task)
+                                                                     (alist-get 'frequency remote-task))
+                                                            (equal (gethash "everyX" local-task)
+                                                                   (alist-get 'everyX remote-task))))
+        (t t))
+       ))
+
+(defun mistkafka/habitica/is-different-between-headline-and-task (local-task remote-task)
+  (not (mistkafka/habitica/is-the-same-between-headline-and-task local-task remote-task)))
+
+;; TODO: 任务状态变更也需要同步。如，本地 TODO --> DONE,而habitica上还是TODO，则需要把这个任务完成
+(defun mistkafka/habitica/sync-to-habitica--depend-on-tasks ()
+  "根据habitica tasks来查找需要同步的headline.
+      1. 拉取habitica上的所有任务(task)
+      2. 根据id，遍历所有task对应的headline，根据headline的类型同步数据
+      3. 如果发现task类型不一致，则删除habitica上的任务，根据新的类型新建一个任务
+      4. 发现habitica上的任务找不到对应的headline，则在 *Inbox* 里新建一个对应的任"
+  (let ((tasks (mistkafka/habitica/get-habitica-tasks)))
+    (mapc
+     (lambda (task)
+       (let* ((task-id (alist-get 'id task))
+              (headline-pos (org-find-property "HABITICA-ID" task-id))
+              (headline-info nil)
+              (local-task nil)
+              (is-type-changed nil)
+              (is-local-status-done-and-remote-todo nil)
+              (is-challenge-task nil))
+         
+         ;; 存在关联的task，则进行同步
+         (if headline-pos
+             (progn 
+               (goto-char headline-pos)
+               (setq headline-info (mistkafka/habitica/get-current-headline-info))
+               (setq local-task (mistkafka/habitica/cover-headline-info-2-habitica-task headline-info))
+               (setq is-challenge-task (alist-get 'challenge task))
+               (setq is-type-changed (not (string= (gethash "type" local-task)
+                                                   (alist-get 'type task))))
+               (setq is-local-status-done-and-remote-todo (and (string= "DONE" (gethash "todo-keyword" headline-info))
+                                                               (not (alist-get 'completed task))))
+               (cond
+                ;; task 类型变更了，则删除task，重新创建
+                ((and is-type-changed (not is-challenge-task)) (progn
+                                   (message (format "任务：%s, 类型变更了，正在删除重建..." task-id))
+                                   (mistkafka/habitica/del-habitica-task-for-current-headline)
+                                   (mistkafka/habitica/create-habitica-task-for-current-headline)))
+                
+                ;; 本地任务已经完成，则把habitica的任务也完成
+                (is-local-status-done-and-remote-todo (progn
+                                                        (message (format "任务：%s, 已经完成！正在将它标记为完成..." task-id))
+                                                        (mistkafka/habitica/make-task-done (alist-get 'id task))))
+
+                ;; 对于chanllenge任务，不能进行除“完成”以外的操作
+                (is-challenge-task (message "任务：%s，是“挑战”任务，无法进行内容修改，跳过！" task-id))
+                
+                ;; task类型、完成状态都没有变，但是其它字段有变更，则进行数据更新
+                ((and (mistkafka/habitica/is-different-between-headline-and-task local-task task)
+                      (not is-challenge-task))  (progn
+                                                                                                (message (format "任务：%s，有字段变更！正在进行同步..." task-id))
+                                                                                                (remhash "type" local-task)
+                                                                                                (mistkafka/habitica/request (format "/tasks/%s" task-id)
+                                                                                                                            "PUT" local-task)))))
+           ;; 完全是新的task，则新增一个headline
+           (progn
+             (when (not (string= "habit" (alist-get 'type task)))
+               (mistkafka/habitica/insert-headline-to-inbox task)
+               )
+             ))
+         ))
+     tasks)
+    (message "根据habitica的数据同步task完成！")))
+
+(defvar mistkafka/habitica/headline-queue-for-new-task nil)
+(defun mistkafka/habitica/sync-to-habitica--depend-on-headlines ()
+  "   从org往habitica同步
+      1. 扫描所有的headline
+      2. 只捕获类型为TODO/NEXT，且没有habitica-id的任务
+      3. 往habitica上新增一个任务"
+  (setq mistkafka/habitica/headline-queue-for-new-task nil)
+  (org-map-tree 'mistkafka/habitica/collect-should-new-tasks)
+  (mistkafka/habitica/create-habitica-task-from-queue)
+  (message "根据headline的数据同步task完成！"))
+
+(defun mistkafka/habitica/collect-should-new-tasks ()
+  (let* ((headline-info (mistkafka/habitica/get-current-headline-info))
+         (habitica-id (gethash 'habitica-id headline-info))
+         (todo-keyword (gethash 'todo-keyword headline-info)))
+    (when (and (seq-contains '("TODO" "NEXT") todo-keyword 'equal)
+               (or (not habitica-id) (s-starts-with? "tmp-uuid" habitica-id)))
+      (setq habitica-id (mistkafka/habitica/gen-headline-tmp-uuid headline-title))
+      (org-set-property "HABITICA-ID" habitica-id)
+      (puthash 'habitica-id habitica-id headline-info)
+      (setq mistkafka/habitica/headline-queue-for-new-task
+            (cons headline-info mistkafka/habitica/headline-queue-for-new-task)))
+    ))
+
+(defun mistkafka/habitica/gen-headline-tmp-uuid (title)
+  (format "tmp-uuid-%s"
+          (md5 (format "%s%s" title (random)))))
+
+(defun mistkafka/habitica/create-habitica-task-from-queue ()
+  (let* ((tasks (mapcar
+                 'mistkafka/habitica/cover-headline-info-2-habitica-task
+                 mistkafka/habitica/headline-queue-for-new-task))
+         (result (alist-get 'data (mistkafka/habitica/request "/tasks/user" "POST" tasks))))
+    (cl-loop for i from 0 below (length result)
+             for headline-info = (nth i mistkafka/habitica/headline-queue-for-new-task)
+             for tmp-id = (gethash 'habitica-id headline-info)
+             for res-data = (if (> (length result) 1)
+                                (aref result i)
+                              result)
+             for id = (alist-get 'id res-data)
+             do (progn
+                  (goto-char (org-find-property "HABITICA-ID" tmp-id))
+                  (org-set-property "HABITICA-ID" id)
+                  (org-set-property "HABITICA-TYPE" (alist-get 'type res-data))))
+    ))
+
+(defun mistkafka/habitica/sync-to-habitica ()
+  (interactive)
+  (mistkafka/habitica/sync-to-habitica--depend-on-tasks)
+  (mistkafka/habitica/sync-to-habitica--depend-on-headlines))
 
 (provide 'mistkafka-habitica)
