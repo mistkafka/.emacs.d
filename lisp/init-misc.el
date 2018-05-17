56c9495e9039e129fb467fd6f0b56538cab7c1a9
mistkafka <mistkafka@gmail.com>
2018-05-17 14:07:06 +0800

    :wheelchair: 将系统的复制黏贴与emacs的kill、yank分隔开来

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -157,15 +157,18 @@
               (define-key typescript-mode-map "@" 'js-doc-insert-tag)))
 
 ;; Apple
+(defun mistkafka/utils/get-region-str ()
+  (buffer-substring-no-properties
+   (region-beginning)
+   (region-end)))
+
 (defun mistkafka/osx/speech ()
   "Speech region content.
 Or word at point.
 Or prompt user input."
   (interactive)
   (let ((content))
-    (cond ((region-active-p) (setq content (buffer-substring-no-properties
-					    (region-beginning)
-					    (region-end))))
+    (cond ((region-active-p) (setq content (mistkafka/utils/get-region-str)))
           ((word-at-point) (setq content (word-at-point)))
           (t (setq content (ivy-read "Speech Content: " nil))))
 
@@ -218,5 +221,28 @@ Or prompt user input."
 ;; window编号
 (require-package 'window-numbering)
 (window-numbering-mode 1)
+
+;; 黏贴板设置，我不喜欢s-c、s-v与emacs的复制黏贴参合在一块
+(setq select-enable-clipboard   nil
+      x-select-enable-clipboard nil)
+
+(defun mistkafka/clipboard/write-clipboard (str)
+  (shell-command-to-string (format "printf \"%s\" | pbcopy" str)))
+
+(defun mistkafka/clipboard/get-paste-str ()
+  (shell-command-to-string "pbpaste"))
+
+(defun mistkafka/clipboard/copy-region ()
+  (interactive)
+  (mistkafka/clipboard/write-clipboard (mistkafka/utils/get-region-str)))
+
+(defun mistkafka/clipboard/paste ()
+  (interactive)
+  (insert (mistkafka/clipboard/get-paste-str)))
+
+(global-unset-key (kbd "s-c"))
+(global-unset-key (kbd "s-v"))
+(global-set-key (kbd "s-c") 'mistkafka/clipboard/copy-region)
+(global-set-key (kbd "s-v") 'mistkafka/clipboard/paste)
 
 (provide 'init-misc)
