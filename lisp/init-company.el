c6048c5e070ba0203eceb042baf9ad3e27daeb83
mistkafka <mistkafka@gmail.com>
2018-08-04 02:18:37 +0800

    :bug: fix company-lsp补全挂掉

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -3,8 +3,9 @@
 
 ;; 配置company，作为lsp的补全前端
 (require-package 'company-lsp)
-(push 'company-lsp company-backends)
+
 (push 'company-yasnippet company-backends)
+(push 'company-lsp company-backends)    ; company-lsp的优先级要放高一点，不然会补全不了
 
 (global-set-key (kbd "M-c") 'company-complete)
 
