2006a5a99e7d3037d27dcb9a1b3aca9f832fe060
zhenguo <zhenguo@glowing.com>
2019-12-12 10:25:43 +0800

    :change:,  disable lsp-ui

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -2,9 +2,10 @@
 (require-package 'lsp-mode)
 (lsp-mode t)
 
-(require-package 'lsp-ui)
-(add-hook 'lsp-mode-hook 'lsp-ui-mode)
-(define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-(define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+;; disable lsp-ui
+;; (require-package 'lsp-ui)
+;; (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+;; (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+;; (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
 
 (provide 'init-lsp)
