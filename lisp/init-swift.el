01dd331c145dd28e113788ae583b9d9a34366f92
zhenguo <zhenguo@glowing.com>
2021-04-25 17:38:02 +0800

    [lsp] 尝试配置lsp-swift

---
+++ lisp/init-swift.el
@@ -0,0 +1,12 @@
+(require-package 'lsp-sourcekit)
+(require-package 'swift-mode)
+
+(use-package lsp-sourcekit
+  :after lsp-mode
+  :config
+  (setq lsp-sourcekit-executable "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp"))
+
+(use-package swift-mode
+  :hook (swift-mode . (lambda () (lsp))))
+
+(provide 'init-swift)
