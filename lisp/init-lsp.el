3510c9bec5bab4c0713111f84da66f9dacbe5fa9
zhenguo <zhenguo@glowing.com>
2021-04-25 17:37:13 +0800

    [lsp] 增加lsp-server的资源，以提高性能

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -8,4 +8,12 @@
 ;; (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
 ;; (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
 
+
+;; 性能
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+(setq lsp-idle-delay 0.500)
+(setq lsp-print-performance t)
+
+
 (provide 'init-lsp)
